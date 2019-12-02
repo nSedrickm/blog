d324748de3209c3a4a79e1ffd8ef8ed06d4f00e6
nSedrickm <nsedrick101@gmail.com>
2019-12-02 16:10:27 +0100

    generating recent_posts from parsing json

--- js/blog.js
+++ js/blog.js
@@ -29,7 +29,9 @@ function dataholder(inputdata) {
 // render HTML
 function renderHTML(data) {
     var post_section = document.getElementById('articles-section');
+    var recent_post_section = document.getElementById('recent_posts');
     var post = '';
+    var recent_posts = '';
     for (let i in data) {
         post += `
             <article class="card my-5 border-0">
@@ -60,9 +62,32 @@ function renderHTML(data) {
                             </div>
 
                         </article>
+            `;
+        recent_posts += `<div class="media mb-2">
+            <img src="${data[i].postImage}" class="mr-3" alt="...">
+            <div class="media-body">
+                <a href="#" class="">
+                    <h4 class="mt-0 bree">${data[i].postTitle}</h4>
+                </a>
+                <p class="montserrat m-0">${data[i].postCaption}</p>
+                <ul class="list-group list-group-horizontal  ">
+                    <li class="list-group-item border-0 pl-0"><a href=" #"><i
+                                class="far fa-clock pr-1"></i>${data[i].postDate}
+                        </a></li>
+                    </li>
+                    <li class="list-group-item border-0 pl-0"><a href=" #">
+                            <i class="far fa-user pr-1"></i>${data[i].postTag}</a></li>
+                    <li class="list-group-item border-0 pl-0"><a href=" #"><i
+                                class="far fa-comments"></i>
+                            03</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
             `
     }
     post_section.innerHTML = post;
+    recent_post_section.innerHTML = recent_posts;
 };
 
 
