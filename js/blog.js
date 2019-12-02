5997a6985f8900385faae99ca03caa177e88f30a
nSedrickm <nsedrick101@gmail.com>
2019-12-02 15:43:12 +0100

    generate post from parsing json

--- js/blog.js
+++ js/blog.js
@@ -1,40 +1,68 @@
 //controls all blog post placements
+$(document).ready(() => {
+    mydata('../MOCK_DATA_SMALL.json',
+        data => {
+            dataholder(data);
+        });
+});
 
-var post_section = document.getElementById("articles-section");
-console.log(post_section);
+// AJAX
+function getJSON(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText)
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
 
-post_section.append(`<article class="card my-5 border-0">
+function mydata(url, callback) {
+    getJSON(url, data => callback(JSON.parse(data)));
+}
 
-<div class="card-img-top">
-    <a href="single-blog.html">
-        <img class="card-img rounded-0" src="img/blog/single_blog_2.png" alt="post photo">
-    </a>
-    <a href="#" class="post_date">
-        <h3>15</h3>
-        <p class="text-white my-2">Jan</p>
-    </a>
-</div>
+function dataholder(inputdata) {
+    console.log("now logging inputdata");
+    console.log(inputdata);
+    renderHTML(inputdata);
+}
 
-<div class="card-body shadow">
-    <a href="single-blog.html">
-        <h2 class="card-title my-4 calistoga">Google inks pact for new 35-storey office
-        </h2>
-    </a>
-    <p class="card-text alata"> That dominion stars lights dominion divide years for
-        fourth
-        have
-        don't stars is
-        that
-        he earth it first without heaven in place seed it second morning saying.</p>
-    <ul class="list-group list-group-horizontal">
-        <li class="list-group-item border-0 pl-0"><a href=" #"><i
-                    class="far fa-user"></i>Travel,
-                Lifestyle</a></li>
-        <li class="list-group-item border-0 pl-0"><a href=" #"><i
-                    class="far fa-comments"></i>
-                03 Comments</a></li>
-        </li>
-    </ul>
-</div>
+// render HTML
+function renderHTML(data) {
+    var post_section = document.getElementById('articles-section');
+    var post = '';
+    for (let i in data) {
+        post += `
+            <article class="card my-5 border-0">
 
-</article>`);
+                            <div class="card-img-top">
+                                <a href="single-blog.html">
+                                    <img class="card-img rounded" src="${data[i].postImage}" alt="post photo">
+                                </a>
+                                <a href="#" class="post_date">
+                                    <h3>${data[i].postDate[0]}</h3>
+                                    <p class="text-white my-2">${data[i].postDate[7]}</p>
+                                </a>
+                            </div>
+
+                            <div class="card-body shadow">
+                                <a href="single-blog.html">
+                                    <h2 class="card-title my-4 calistoga">${data[i].postTitle}</h2>
+                                </a>
+                                <p class="card-text alata">${data[i].postDetails}</p>
+                                <ul class="list-group list-group-horizontal">
+                                    <li class="list-group-item border-0 pl-0"><a href=" #"><i
+                                                class="far fa-user pr-1"></i>${data[i].postTag}</a></li>
+                                    <li class="list-group-item border-0 pl-0"><a href=" #">
+                                    <i class="far fa-comments"></i>
+                                            03 Comments</a></li>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </article>
+            `
+    }
+    post_section.innerHTML = post;
+};
+
+
